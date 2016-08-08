95adc2b4baf2478ac9f7fe16a17fb1039d6e4c8b
wesleymacente <wesleymacente@gmail.com>
2016-08-08 15:38:27 -0400

    Add files via upload

---
+++ sender.php
@@ -0,0 +1,43 @@
+<?php 
+// Replace with real BROWSER API key from Google APIs
+$apiKey = "AIzaSyBBziv2dYAqWDjEV-56WTteiNUlIkZCPFQ";
+
+// Replace with real client registration IDs 
+$registrationIDs = array( "123", "456" );
+
+// Message to be sent
+$message = "xsss";
+
+// Set POST variables
+$url = 'https://android.googleapis.com/gcm/send';
+
+$fields = array(
+                'registration_ids'  => $registrationIDs,
+                'data'              => array( "message" => $message ),
+                );
+
+$headers = array( 
+                    'Authorization: key=' . $apiKey,
+                    'Content-Type: application/json'
+                );
+
+// Open connection
+$ch = curl_init();
+
+// Set the url, number of POST vars, POST data
+curl_setopt( $ch, CURLOPT_URL, $url );
+
+curl_setopt( $ch, CURLOPT_POST, true );
+curl_setopt( $ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
+
+curl_setopt( $ch, CURLOPT_POSTFIELDS, json_encode( $fields ) );
+
+// Execute post
+$result = curl_exec($ch);
+
+// Close connection
+curl_close($ch);
+
+echo $result;
+ ?>
