34d71ede2af8acfdb55d967552930096df7e0476
wesleymacente <wesleymacente@gmail.com>
2016-08-08 17:18:24 -0400

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,17 @@
+if ('serviceWorker' in navigator) {
+  console.log('Service Worker is supported');
+  navigator.serviceWorker.register('service-worker.js').then(function() {
+    return navigator.serviceWorker.ready;
+  }).then(function(reg) {
+    console.log('Service Worker is ready :^)', reg);
+    reg.pushManager.subscribe({userVisibleOnly: true}).then(function(sub) {
+      console.log('endpoint:', sub.endpoint);
+    });
+  }).catch(function(error) {
+    console.log('Service Worker error :^(', error);
+  });
+}
+
 console.log('Started', self);
 
 self.addEventListener('install', function(event) {
